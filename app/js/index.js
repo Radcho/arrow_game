354d05314f05b3a2b1d67a826bb7946ce225eaf4
Radovan Balog <radovanbalog@gmail.com>
2019-12-11 21:32:36 +0100

    sprites type, canvas and basic server

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,22 @@
-const a = new Sprite(null, null, 0, 0);
+window.addEventListener('load', main);
+
+/**
+ * @type {HTMLCanvasElement}
+ */
+let canvas;
+
+function main() {
+    canvas = document.querySelector('canvas');
+    window.addEventListener('resize', resizeCanvas);
+    resizeCanvas();
+}
+
+function resizeCanvas() {
+    let section = document.querySelector('.main');
+    canvas.width = section.clientWidth;
+    canvas.style.width = section.clientWidth + 'px';
+    canvas.height = section.height;
+    canvas.style.height = section.clientHeight + 'px';
+}
+
+
